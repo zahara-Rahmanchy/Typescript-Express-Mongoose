78493b094ca30752770c000e3c325762d9ff139c
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-11-23 09:24:06 +0600

    user schema created

---
+++ src/app/modules/UserModule/UserInterface.ts
@@ -0,0 +1,29 @@
+export type FullName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type Orders = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+  fullname: FullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: Address;
+  orders?: Orders[];
+};
---
+++ src/app/modules/UserModule/Usermodel.ts
@@ -0,0 +1,61 @@
+import { Schema, model } from 'mongoose';
+import { Address, FullName, Orders, User } from './UserInterface';
+
+const FullNameSchema = new Schema<FullName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const OrdersSchema = new Schema<Orders>({
+  productName: {
+    type: String,
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const AddressSchema = new Schema<Address>({
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserSchema = new Schema<User>({
+  userId: { type: Number },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  fullname: FullNameSchema,
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean },
+  hobbies: { String },
+
+  address: AddressSchema,
+  orders: OrdersSchema,
+});
+
+export const UserModel = model<User>('User', UserSchema);
